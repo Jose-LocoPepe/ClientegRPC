9739553a3a5acb31bb358349ec7c6cf7455037f4
Ertinho <105566974+Ertinho@users.noreply.github.com>
2024-12-22 01:35:52 -0300

    feat: add update client's days

--- Services/GreeterService.cs
+++ Services/GreeterService.cs
@@ -1,29 +1,36 @@
 using Grpc.Core;
 using ClientegRPC;
-
+using ClientegRPC.Models;
 namespace ClientegRPC.Services;
 
 public class GreeterService : Greeter.GreeterBase
 {
-    public override Task<AgregarReply> AgregarCliente(AgregarRequest request, ServerCallContext context)
-    {
-        // Aquí se puede agregar la lógica para agregar el cliente a la base de datos
-
-        return Task.FromResult(new AgregarReply
-        {
-            Message = "Cliente added successfully!"
-        });
-
-
-    }
+    // Esto debería ir en el repositorio de ServidorgRPC
+    // public override Task<AgregarReply> AgregarCliente(AgregarRequest request, ServerCallContext context)
+    // {
+    //     // Aquí se puede agregar la lógica para agregar el cliente a la base de datos
+    //     return Task.FromResult(new AgregarReply
+    //     {
+    //         Message = "Cliente added successfully!"
+    //     });
+    // }
 
     public override Task<ReservaReply> Reservar(ReservaRequest request, ServerCallContext context)
     {
+        ClientesContext clientesContext = new();
+        var cliente = clientesContext.Clientes.Find(request.IdCliente);
+
+        if (cliente == null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound, "Cliente no encontrado"));
+        }
+
+        cliente.Dias = request.Dias;
+        clientesContext.SaveChanges();
+
         return Task.FromResult(new ReservaReply
         {
-            
+            Message = "Cliente actualizado exitosamente!"
         });
-
-
     }
 }
